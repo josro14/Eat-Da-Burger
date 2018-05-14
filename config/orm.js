b29a7c42d81734c62cfb428a2d4099b13ca66006
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-05-14 00:01:12 -0600

    created all necessary files for burger eating app. Lots of code to still be written to make this thing work

---
+++ config/orm.js
@@ -0,0 +1,5 @@
+var connection = require("../config/connection.js");
+
+// write code here to create methods for storing and updating information to the database
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("swiss mushroom triple burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("all-american classic", false);
